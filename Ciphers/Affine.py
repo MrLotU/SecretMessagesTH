73f3e0be94cd9789830a66412f65bfda3ebfc1c9
Jari Koopman <j.koopman@jarict.nl>
2018-04-29 14:17:51 +0200

    Implement Affine Cipher

--- Ciphers/Affine.py
+++ Ciphers/Affine.py
@@ -2,3 +2,41 @@ from cipher import Cipher
 
 class Affine(Cipher):
     name = 'Affine'
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def alphabet(self):
+        alphabet = {}
+        for i in range(26):
+            alphabet[chr(i+65)] = chr(((self.a*i+self.b)%26)+65)
+        return alphabet
+
+    def egcd(self, a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = self.egcd(b % a, a)
+            return (g, x - (b // a) * y, y)
+
+    def modinv(self):
+        g, x, y = self.egcd(self.a, 26)
+        if g != 1:
+            raise Exception('modular inverse does not exist')
+        else:
+            return x % 26
+    
+    def encrypt(self, msg):
+        msg = msg.upper()
+        out = []
+        for char in msg:
+            out.append(chr(((self.a * (ord(char) - 65) + self.b) % 26) + 65))
+        return ''.join(out)
+
+    def decrypt(self, msg):
+        msg = msg.upper()
+        out = []
+        for char in msg:
+            out.append(chr((self.modinv() * ((ord(char) - 65) - self.b) % 26) + 65))
+        return ''.join(out)
--- secret_message.py
+++ secret_message.py
@@ -37,5 +37,6 @@ def setup():
         return
     print('Cool! Let\'s get started!')
 
+
 if __name__ == '__main__':
     setup()
